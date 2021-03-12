bfff374523d3ad664d4f485d9b0e0325395e378d
Samuel Madeira <spmadeira@unifor.br>
2021-03-12 01:05:00 -0300

    add more formats

---
+++ View/Button.js
@@ -0,0 +1,6 @@
+let counter = 0;
+
+function count(){
+    counter++;
+    document.getElementById('count_text').innerText = String(counter);
+}
--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import socket
 from threading import Thread
 import os.path
 
+HOST = '0.0.0.0'
+PORT = 6789
+
+SERVE = dict({
+    'html': 'text/html',
+    'ico': 'image/x-icon',
+    'jpeg': 'image/jpeg',
+    'png': 'image/png',
+    'js': 'text/javascript'
+})
+
 class HttpMessage(object):
     def __init__(self, method, route, headers, body):
         self.method = method
@@ -11,7 +22,15 @@ class HttpMessage(object):
 
 
 def parse(http):
-    heading, body = http.split('\r\n\r\n')
+    parts = http.split('\r\n\r\n')
+
+    if len(parts) > 1:
+        heading = parts[0]
+        body = parts[1]
+    else:
+        heading = parts[0]
+        body = '' 
+
     info = heading.split('\r\n')
     method, route, version = info.pop(0).split(' ')
     headers = { }
@@ -21,38 +40,49 @@ def parse(http):
 
     return HttpMessage(method, route, headers, body)
 
-
+    
 def respond(msg):
     if not msg.method == 'GET':
         print('{0} != GET'.format(msg.method))
-        return 'HTTP/1.0 405 Method Not Allowed\ncontent-type: application/problem'
+        return 'HTTP/1.0 405 Method Not Allowed\ncontent-type: application/problem'.encode()
+
     route = msg.route[1:]
 
     if not route:
         route = 'Index'
 
-    file_name = './View/{0}.html'.format(route)
+    if len(route.split('.')) == 1: # If has no defined file format
+        file_name = './View/{0}.html'.format(route)
+    else: 
+        file_name = './View/{0}'.format(route)
+
+    requested_format = file_name.split('.')[-1]
+
+    if not requested_format in dict.keys(SERVE):
+        'HTTP/1.0 500 Internal Server Error\ncontent-type: application/problem'.encode()
 
     if os.path.isfile(file_name):
         try:
-            f = open('./View/{0}.html'.format(route))
+            f = open(file_name, "rb")
             content = f.read()
-            return 'HTTP/1.0 200 OK\ncontent-type: text/html\n\n{0}'.format(content)
+            return 'HTTP/1.0 200 OK\ncontent-type: {0}\n\n'.format(SERVE[requested_format]).encode()+content
         except IOError:
-            return 'HTTP/1.0 500 Internal Server Error\ncontent-type: application/problem'
-        finally:
+            return 'HTTP/1.0 500 Internal Server Error\ncontent-type: application/problem'.encode()
+        else:
             f.close()
     else:
-        return 'HTTP/1.0 404 Not Found\ncontent-type: application/problem\n\nFile Not Found'
+        return 'HTTP/1.0 404 Not Found\ncontent-type: application/problem\n\nFile Not Found'.encode()
 
 
 def handle_request(req, conn):
-    res = respond(parse(req)).encode()
+    msg = parse(req)
+    print(msg.route)
+    res = respond(msg)
     conn.sendall(res)
     conn.close()
 
-HOST = '0.0.0.0'
-PORT = 54321
+
+#####
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
